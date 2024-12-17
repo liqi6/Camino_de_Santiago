c99a62d028f292f118448a80879e20188b467040
chinchillaZ <145755529+chinchillaZ@users.noreply.github.com>
2024-12-17 23:59:47 +0800

    Update 1_🌍_Interactive_Map.py

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -25,17 +25,30 @@ with col2:
 
 with col1:
 
+    # m = leafmap.Map(
+    #     locate_control=True, latlon_control=True, draw_export=True, minimap_control=True, center = [42.5, -4.0], zoom = 7 , minimap_control=True
+    # )
+    # #m.add_basemap(basemap)
+    # m = leafmap.Map(minimap_control=True)
+    # #m.add_basemap("OpenTopoMap")
+    # m.to_streamlit(height=700)
+    # Create the map
     m = leafmap.Map(
-        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True, center = [42.5, -4.0], zoom = 7 , minimap_control=True
+        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True,
+        center=[42.5, -4.0], zoom=7
     )
-    #m.add_basemap(basemap)
-    m = leafmap.Map(minimap_control=True)
-    #m.add_basemap("OpenTopoMap")
-    # m = leafmap.Map(center = [42.5, -4.0], zoom = 7 , minimap_control=True)
+
+    # Clear previous layers if any
+    m.clear_layers()
+
     # Add GeoJSON line to the map
-    geojson_url = "https://chinchillaz.github.io/streamlit-hw/all_Camino_route.geojson"
+    geojson_url = "https    ://chinchillaz.github.io/streamlit-hw/all_Camino_route.geojson"
 
     # Define a style function to set line color to navy
     style = {"color": "navy", "weight": 3, "opacity": 0.8}
+
+    # Add the GeoJSON to the map with the defined style
     m.add_geojson(geojson_url, layer_name="Camino de Santiago Route", style=style)
+
+    # Display the map in streamlit
     m.to_streamlit(height=700)
