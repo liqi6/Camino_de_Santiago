66d89ac81b08aa00c795add57b787105ebb24fb8
chinchillaZ <145755529+chinchillaZ@users.noreply.github.com>
2024-12-18 00:05:18 +0800

    Update 1_🌍_Interactive_Map.py

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -26,9 +26,15 @@ with col2:
 with col1:
 
     m = leafmap.Map(
-        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True, center = [42.5, -4.0], zoom = 7 
+        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True, center = [42.5, -4.0], zoom = 8 
     )
     m.add_basemap(basemap)
+
+    geojson_url = "https://chinchillaz.github.io/streamlit-hw/all_Camino_route.geojson"
+    # Define a style function to set line color to navy
+    style = {"color": "navy", "weight": 3, "opacity": 0.8}
+    m.add_geojson(geojson_url, layer_name="Camino de Santiago Route", style=style)
+    
     m.to_streamlit(height=700)
 
 
